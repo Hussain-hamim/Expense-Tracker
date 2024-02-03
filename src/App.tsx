37c27b79fd98a4d8e9967fc5ad6a801a88ba1f5d
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-02-03 10:09:04 +0430

    exercise: deeply nested object and array.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import { useState } from "react";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    topping: ["Mushroom "],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "product 1", quantity: 2 },
+      { id: 2, title: "product 2", quantity: 2 },
+    ],
   });
 
   function handleClick() {
-    setPizza({ ...pizza, topping: [...pizza.topping, "Cheese "] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: 3 } : item
+      ),
+    });
   }
 
   return (
     <>
-      <div>{pizza.topping}</div>
+      <div>
+        {cart.items.map((item) =>
+          item.id === 1 ? <p key={item.id}>{item.quantity}</p> : ""
+        )}
+      </div>
       <button onClick={handleClick}>Change</button>
     </>
   );
